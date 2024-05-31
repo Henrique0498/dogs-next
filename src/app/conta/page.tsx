154adb4dbf64089dc9d8d17a0d8a4677609b0f16
Henrique Lopes <h.lopes.silva2015@gmail.com>
2024-05-31 17:35:52 -0300

    Criação da pagina minha conta.

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,15 +1,40 @@
-import { useUser } from "@/context/userContext";
+import photosGet from "@/actions/photosGet";
+import userGet from "@/actions/userGet";
+import Feed from "@/components/Feed";
 
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Minha Conta",
 };
 
-export default function ContaPage() {
+export default async function ContaPage() {
+  const { data: user } = await userGet();
+  const { data } = await photosGet({ user: user?.username });
+
   return (
     <main>
-      <h1>Conta</h1>
+      {data?.length ? (
+        <Feed photos={data} />
+      ) : (
+        <div>
+          <p
+            style={{ color: "#444", fontSize: "1.25rem", marginBottom: "1rem" }}
+          >
+            Não tem nenhuma foto.
+          </p>
+          <Link
+            href={"/conta/postar"}
+            className="button"
+            style={{
+              display: "inline-block",
+            }}
+          >
+            Postar foto
+          </Link>
+        </div>
+      )}
     </main>
   );
 }
